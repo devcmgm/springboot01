15cede31a1c3dbb7a91105991e4643cbeae2132e
Zachary Lewis <devcmontgomery@gmail.com>
2020-02-04 14:43:20 -0600

    k

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # springboot01
+
+<h1>Spring Boot Simple Demo</h1>
+<P>
+Simple app with a login user and password and in memory database
+</P>
---
+++ src/main/java/com/devcmgm/reactdemo/backenddemo/db/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.devcmgm.reactdemo.backenddemo.db;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;;
+
+@RepositoryRestResource
+public interface PersonRepository extends CrudRepository <Person, Long> {
+    
+
+}
